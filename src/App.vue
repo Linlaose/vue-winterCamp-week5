54c156739fb9b0e679c763c2d8c3eb1b0d000411
Ryan Tsai <a0986575797@gmail.com>
2023-02-06 16:44:18 +0800

    初始版面

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<script>
+import UserProductModalComponent from "./components/UserProductModal/UserProductModalComponent.vue";
+import ProductsComponent from "./components/Products/ProductsComponent.vue";
+import CartsComponent from "./components/Carts/CartsComponent.vue";
+import FormComponent from "./components/Form/FormComponent.vue";
+
+export default {
+  components: {
+    ProductsComponent,
+    UserProductModalComponent,
+    CartsComponent,
+    FormComponent,
+  },
+  methods: {},
+};
+</script>
+
+<template>
+  <div class="container">
+    <div class="mt-4">
+      <user-product-modal-component />
+      <products-component />
+      <carts-component />
+      <form-component />
+    </div>
+  </div>
+</template>
